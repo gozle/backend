3e938cd7629a7072e6826cb6b230ee946070547d
QBT547 <khydyrov547@gmail.com>
2025-01-05 09:17:36 +0500

    initial_2

--- _/feed.py
+++ _/feed.py
@@ -9,6 +9,24 @@ feed = feedparser.parse(rss_url_2)
 
 if feed.status == 200:
     for entry in feed.entries:
-        print(entry.keys())
+        # print(entry.keys())
+
+        title = entry.get("title", "")
+        summary = entry.get("summary", "")
+
+        if entry.get("content", ""):
+            content =  entry.get('content', '')
+
+        if entry.get("media_content", ""):
+            image = entry.get("media_content", "")[0]
+
+        link = "link_of_item",entry.get("link", "")
+        
+        source_name = feed.feed.get('title', '')
+        source_link = feed.feed.get('link', '')
+        source_language = feed.feed.get('language', '')
+
+        source_icon = feed.feed.image.get('url', '')
+
 else:
     print('smth went wrong')
